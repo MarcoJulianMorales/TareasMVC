bf703d7864298cae7add968e336a548bd364a941
Marco Julian Morales Leyva <marcojulian.morales@gmail.com>
2023-09-04 16:46:09 -0700

    Video 238 agregando tarea en memoria

---
+++ TareasMVC/wwwroot/js/tareas.js
@@ -0,0 +1,16 @@
+﻿function agregarNuevaTarea() {
+    tareaListadoViewModel.tareas.push(new tareaElementoListadoViewModel({ id: 0, titulo: '' }));
+
+    $("[name=titulo-tarea]").last().focus();
+}
+
+function manejarFocusOutTituloTarea(tarea) {
+    const titulo = tarea.titulo();
+
+    if (!titulo) {
+        tareaListadoViewModel.tareas.pop();
+        return;
+    }
+
+    tarea.id(1);
+}
